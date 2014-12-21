7b125ffec8afcc9f52dce3148fd6f62ea3a35bc7
mtfly <915261512@qq.com>
2014-12-21 13:06:02 +0800

    Update getpic.py

--- getpic.py
+++ getpic.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-#coding:utf-8
-__author__ = 'mtfly'
+#!/usr/bin/python
+#coding=utf-8
 
 import requests
-n=raw_input('input the school number:')
-url=r'http://my.zzti.edu.cn/attachmentDownload.portal?notUseCache=true&type=userFace&ownerId='+n
-headers={'Cookie': r'*'}
-a=requests.get(url,headers=headers)
+
+n = raw_input('input the school number:')
+url = r'http://my.zzti.edu.cn/attachmentDownload.portal?notUseCache=true&type=userFace&ownerId=' + n
+headers = {'Cookie': r'*'}
+
+r_get = requests.get(url, headers=headers)
 with open("get.jpg", "wb") as code:
      code.write(a.content)
